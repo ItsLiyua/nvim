bb26967317d409615e2d7fe125643860aedf9f65
Liyua <liyua@duck.com>
2024-05-24 22:08:24 +0200

    Hidden files are now visible in neo-tree.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,13 @@
 local setup = function()
+  require('neo-tree').setup({
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  })
   vim.keymap.set('n', '<C-n>', ':Neotree focus<CR>')
 end
 
