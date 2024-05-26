04245bf0cac43ca2f62536c8ae0f5eb88a388837
Liyua <liyua@duck.com>
2024-05-26 18:13:43 +0200

    Added line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,7 +2,11 @@ vim.cmd('set expandtab')
 vim.cmd('set tabstop=2')
 vim.cmd('set softtabstop=2')
 vim.cmd('set shiftwidth=2')
+
 vim.cmd('set termguicolors')
+
+vim.cmd('set number')
+vim.cmd('set relativenumber')
 
 vim.g.mapleader = ' '
 
