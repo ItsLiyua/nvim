df487a0b8b5eaf292e5bb47e67111168792b35eb
Liyua <liyua@duck.com>
2024-05-27 23:06:58 +0200

    Added border to docs.

--- lua/plugins/language-config.lua
+++ lua/plugins/language-config.lua
@@ -41,15 +41,30 @@ return {
     'neovim/nvim-lspconfig',
     name = 'nvim-lspconfig',
     config = function()
+      local border = {
+        { "🭽", "FloatBorder" },
+        { "▔", "FloatBorder" },
+        { "🭾", "FloatBorder" },
+        { "▕", "FloatBorder" },
+        { "🭿", "FloatBorder" },
+        { "▁", "FloatBorder" },
+        { "🭼", "FloatBorder" },
+        { "▏", "FloatBorder" },
+      }
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local handlers = {
+        ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+        ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signatureHelp, { border = border }),
+      }
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.java_language_server.setup({ capabilities = capabilities })
-      lspconfig.kotlin_language_server.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities })
-      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({ capabilities = capabilities, handlers = handlers })
+      lspconfig.java_language_server.setup({ capabilities = capabilities, handlers = handlers })
+      lspconfig.kotlin_language_server.setup({ capabilities = capabilities, handlers = handlers })
+      lspconfig.html.setup({ capabilities = capabilities, handlers = handlers })
+      lspconfig.cssls.setup({ capabilities = capabilities, handlers = handlers })
       lspconfig.ltex.setup({
         capabilities = capabilities,
+        handlers = handlers,
         settings = {
           ltex = {
             language = 'de-DE',
@@ -58,10 +73,10 @@ return {
       })
       lspconfig.marksman.setup({ capabilities = capabilities })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})                         -- Show docs
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})                 -- Show docs
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})           -- Show definition
       vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, {})          -- Show declaration
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {}) -- Code Adction
+      vim.keymap.set({ 'n', 'v' }, '<leader>ga', vim.lsp.buf.code_action, {}) -- Code Adction
       vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})               -- Code Formatting
     end,
   },
