adb4982697d47815027f09e3b4d61fcf23f12814
Liyua <liyua@duck.com>
2024-09-13 19:22:22 +0200

    Added autotag support for html and some other languages

---
+++ lua/liyua/plugins/autotag.lua
@@ -0,0 +1,13 @@
+return {
+  "windwp/nvim-ts-autotag",
+  lazy = true,
+  event = "InsertEnter",
+  ft = {
+    "html",
+    "javascript",
+    "jsx",
+    "markdown",
+    "typescript",
+    "xml",
+  }
+}
