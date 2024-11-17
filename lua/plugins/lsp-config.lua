6121cf32fcef2b8208def01fc4f20834a01acb03
Liyua <liyua@duck.com>
2024-11-17 23:23:02 +0100

    Added lsp support.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,15 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.kotlin_language_server.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v", }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, {})
     end,
   }
 }
