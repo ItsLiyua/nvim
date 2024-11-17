df560d443f840894e69180cf50d352f2826cef48
Liyua <liyua@duck.com>
2024-11-17 23:02:15 +0100

    Initial commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "kotlin_language_server",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.kotlin_language_server.setup({})
+    end,
+  }
+}
