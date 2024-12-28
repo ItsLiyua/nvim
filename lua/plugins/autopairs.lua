d21d1bc65c2486c6ed44a17b5f4cb08d22f5f9e0
Liyua <liyua@duck.com>
2024-12-28 19:26:39 +0100

    Added markdown rendering in nvim, a better autopairs plugin and a git graph display

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,21 +1,6 @@
 return {
-  "windwp/nvim-autopairs",
-  config = function()
-    local autopairs = require("nvim-autopairs")
-    autopairs.setup({
-      check_ts = true,
-      ts_config = {
-        lua = { "string" },
-        javascript = { "template_string" },
-        java = false,
-      },
-    })
-
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    local cmp = require("cmp")
-
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
-  event = { "InsertEnter" },
-  dependencies = { "hrsh7th/nvim-cmp" },
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = { "BufReadPre", "BufNewFile" },
+  config = true,
 }
