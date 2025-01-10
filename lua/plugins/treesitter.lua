732b6ce0ccbe84123f85740ff8b393f16a3637c1
Liyua <liyua@duck.com>
2025-01-10 11:45:06 +0100

    Added markdown inline treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
 
         "latex",
         "markdown",
+        "markdown_inline",
 
         "html",
         "css",
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
   ft = { "tex" },
   init = function()
     vim.g.vimtex_view_method = "zathura"
+    -- vim.g.vimtex_syntax_conceal_disable = true
     vim.g.vimtex_syntax_conceal = {
       math_fracs = false
     }
