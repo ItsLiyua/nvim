24e4d8d41c14b1b97d50ef8f9a41e97c69be24e0
Liyua <liyua@duck.com>
2025-01-10 11:28:55 +0100

    Added markdown preview plugin back but without autostart

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,24 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  ft = { "markdown" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("render-markdown").enable()
+    end,
   },
-  config = function()
-    require("render-markdown").enable()
-  end,
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_theme = "dark"
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
