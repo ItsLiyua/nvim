b8aea975bdebe0d6a8c860d2d5a46a6fd55f860f
Liyua <liyua@duck.com>
2025-01-22 10:06:27 +0100

    Removed neodev since it's archived now

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,8 +6,18 @@ return {
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_syntax_conceal = {
       math_fracs = false,
+      accents = true,
+    }
+    vim.g.vimtex_syntax_custom_cmds = {
     }
     vim.g.vimtex_syntax_custom_cmds_with_concealed_delims = {
+      {
+        mathmode = true,
+        name = "vv",
+        concealchar = "v",
+        conceal = true,
+        optconceal = true,
+      },
       {
         mathmode = true,
         name = "br",
