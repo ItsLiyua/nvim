900a74809c3b3cf8c4e9fb3dbb81f57b78ef1a0f
Liyua <liyua@duck.com>
2025-01-26 17:14:24 +0100

    Removed two plugins I did not use

--- lua/plugins/menu.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvzone/menu",
-  dependencies = { "nvzone/volt" },
-  lazy = true,
-  keys = { { "<C-t>", function() require("menu").open("default") end, desc = "Open context menu" } }
-}
