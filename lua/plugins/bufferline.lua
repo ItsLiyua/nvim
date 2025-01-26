12b96c1b081f0a9ca544fc79bdc4e70c4dd47137
Liyua <liyua@duck.com>
2025-01-26 14:51:11 +0100

    Added keybinds to jump to buffer directly via space + number

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,13 +1,14 @@
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
-	dependencies = { "nvim-tree/nvim-web-devicons", "RedsXDD/neopywal.nvim" },
-	-- lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons", "RedsXDD/neopywal.nvim", "folke/which-key.nvim" },
 	event = { "BufAdd", "BufEnter", "BufReadPre", "BufNewFile" },
 	config = function()
 		require("bufferline").setup({
 			options = {
-				seperator_style = "slant",
+				-- separator_style = "slope",
+				indicator = { style = "underline" },
+				diagnostics = "nvim_lsp",
 				offsets = {
 					{
 						filetype = "neo-tree",
@@ -16,8 +17,13 @@ return {
 						text_align = "center",
 					},
 				},
+				show_buffer_icons = true,
 			},
-			highlights = require("neopywal.theme.plugins.bufferline").setup(),
 		})
+
+		local wk = require("which-key")
+		for i = 1, 10 do
+			wk.add({ "<leader>" .. i, "<cmd>BufferLineGoToBuffer " .. i .. "<CR>", hidden = true })
+		end
 	end,
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,50 +1,53 @@
 return {
-  "RedsXDD/neopywal.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    use_wallust = true,
-    transparent_background = true,
-    plugins = {
-      dropbar = {
-        enabled = true,
-        colored_text = true,
-      },
-      lazy = true,
-      mason = true,
-      markdown = true,
-      neotree = true,
-      nvim_cmp = true,
-      lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "bold", "italic" },
-          hints = { "bold", "italic" },
-          information = { "bold", "italic" },
-          ok = { "bold", "italic" },
-          warnings = { "bold", "italic" },
-          unnecessary = { "bold", "italic" },
-        },
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          information = { "undercurl" },
-          ok = { "undercurl" },
-          warnings = { "undercurl" },
-        },
-        inlay_hints = {
-          background = true,
-          style = { "bold", "italic" },
-        },
-      },
-      surround = true,
-      treesitter = true,
-      telescope = { enabled = true, style = "nvchad" },
-      trouble = true,
-      which_key = true,
-    },
-  },
-  init = function()
-    vim.cmd.colorscheme("neopywal-dark")
-  end,
+	"RedsXDD/neopywal.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		use_wallust = true,
+		transparent_background = true,
+		plugins = {
+			dashnoard = true,
+			dropbar = {
+				enabled = true,
+				colored_text = true,
+			},
+			lazy = true,
+			mason = true,
+			markdown = true,
+			neotree = true,
+			nvim_cmp = true,
+			noice = true,
+			notify = true,
+			lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "bold", "italic" },
+					hints = { "bold", "italic" },
+					information = { "bold", "italic" },
+					ok = { "bold", "italic" },
+					warnings = { "bold", "italic" },
+					unnecessary = { "bold", "italic" },
+				},
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					information = { "undercurl" },
+					ok = { "undercurl" },
+					warnings = { "undercurl" },
+				},
+				inlay_hints = {
+					background = true,
+					style = { "bold", "italic" },
+				},
+			},
+			surround = true,
+			treesitter = true,
+			telescope = { enabled = true, style = "nvchad" },
+			trouble = true,
+			which_key = true,
+		},
+	},
+	init = function()
+		vim.cmd.colorscheme("neopywal-dark")
+	end,
 }
