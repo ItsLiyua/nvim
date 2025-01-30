f5facf98bd745d07536d71e0ad89b525f6a82c85
Liyua <liyua@duck.com>
2025-01-30 23:10:19 +0100

    Fixed an issue with the session system

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,11 +1,18 @@
 return {
 	"rmagatti/auto-session",
+	init = function()
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+	end,
 	opts = {
-		auto_restore_enabled = true,
+		auto_restore = false,
+		auto_save = true,
+		auto_create = true,
 		auto_session_suppress_dirs = { "~/", "~/Code", "~/Downloads", "~/Documents", "~/Desktop" },
 	},
 	keys = {
 		{ "<leader>qr", "<cmd>SessionRestore<CR>", desc = "Restore previous session" },
 		{ "<leader>qs", "<cmd>SessionSave<CR>", desc = "Save current session" },
 	},
+	cmd = { "SessionRestore", "SessionSave" },
+	event = { "ExitPre" },
 }
