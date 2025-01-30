1ef476c5f5fa0b19fc2c5bd7e08bdfd756d02c92
Liyua <liyua@duck.com>
2025-01-30 18:06:41 +0100

    Fixed a bug where the auto-save plugin tries to save nun-modifiable buffers like telescope prompts and so on

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -4,8 +4,14 @@ return {
 	keys = { { "<leader>mt", "<cmd>ASToggle<CR>", desc = "Toggle autosave" } },
 	opts = {
 		debounce_delay = 5000,
-    condition = function(buf)
-      return not vim.api.nvim_buf_get_name(buf) == "zuzu///editor"
-    end
+		condition = function(buf)
+			local fn = vim.fn
+			local utils = require("auto-save.utils.data")
+
+			if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then -- If buffer is writable
+				return not vim.api.nvim_buf_get_name(buf) == "zuzu///editor" -- Check if buffer is a zuzu configuration editor
+			end
+			return false
+		end,
 	},
 }
