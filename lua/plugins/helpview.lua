9a4f83db742bdfc3124bda7b76948d4abd525963
Liyua <liyua@duck.com>
2025-02-05 22:34:59 +0100

    Modified config of helpview plugin. I need to lazy load this som way

--- lua/plugins/helpview.lua
+++ lua/plugins/helpview.lua
@@ -1,8 +1,6 @@
 return {
 	"OXY2DEV/helpview.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		preview = {
 			icon_provider = "devicons",
