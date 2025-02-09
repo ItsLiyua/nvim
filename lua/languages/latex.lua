31c8d4687454e1fe4f917a450c8586d38a05df8f
Liyua <liyua@duck.com>
2025-02-09 14:51:57 +0100

    Fixed ltex not starting when java is not set to the correct version

--- lua/languages/latex.lua
+++ lua/languages/latex.lua
@@ -1,19 +1,20 @@
 return require("api.language").new({
-	ft = { "tex" },
-	treesitter = "latex",
-	formatters = {
-		latexindent = {},
-	},
-	lsp = {
-		{
-			name = "ltex",
-			config = {
-				settings = {
-					ltex = {
-						language = "en-US",
-					},
-				},
-			},
-		},
-	},
+  ft = { "tex" },
+  treesitter = "latex",
+  formatters = {
+    latexindent = {},
+  },
+  lsp = {
+    {
+      name = "ltex",
+      config = {
+        cmd = { "env", "JAVACMD=/usr/lib64/jvm/java-21-openjdk/bin/java", vim.fn.stdpath("data") .. "/mason/bin/ltex-ls" },
+        settings = {
+          ltex = {
+            language = "en-US",
+          },
+        },
+      },
+    },
+  },
 })
