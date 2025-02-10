cc36c5b09d815e6f8943e4cd9e702ccb7f41b596
Liyua <liyua@duck.com>
2025-02-10 19:44:57 +0100

    Added runner display strat that doesn't take up half the screen

--- lua/languages/java.lua
+++ lua/languages/java.lua
@@ -1,12 +1,21 @@
 return require("api.language").new({
   ft = { "java" },
   treesitter = "java",
-  formatters = { ["google-java-format"] = {} },
+  -- formatters = { ["google-java-format"] = {} },
   lsp = {
     {
       name = "jdtls",
       config = {
-        cmd = { vim.fn.stdpath("data") .. "/mason/packages/jdtls/jdtls", "--java-executable=/usr/lib64/jvm/java-21-openjdk/bin/java" }
+        cmd = { vim.fn.stdpath("data") .. "/mason/packages/jdtls/jdtls", "--java-executable=/usr/lib64/jvm/java-21-openjdk/bin/java" },
+        settings = {
+          java = {
+            format = {
+              settings = {
+                profile = "GoogleStyle"
+              }
+            }
+          }
+        }
       },
     },
   },
