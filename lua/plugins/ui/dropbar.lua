d6d34631eb6b0c9d10b996f65adc2b2613cfd722
Liyua <liyua@duck.com>
2025-02-13 09:01:39 +0100

    Removed common paths like "src/main/java" or "lua" from breadcrumbs.

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -1,11 +1,47 @@
 return {
-  "Bekaboo/dropbar.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-    },
-    "nvim-tree/nvim-web-devicons",
-  },
+	"Bekaboo/dropbar.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		sources = {
+			path = {
+				relative_to = function(_, win)
+					local filter = {
+						"src/main/java",
+						"src/main/kotlin",
+						"src/client/java",
+						"src/client/kotlin",
+						"lua",
+					}
+					local pos_filters = {}
+
+					local ok, cwd = pcall(vim.fn.getcwd, win)
+					if ok then
+						for _, f in pairs(filter) do
+							if vim.fn.isdirectory(cwd .. "/" .. f) ~= 0 then
+								pos_filters[#pos_filters + 1] = cwd .. "/" .. f
+							end
+						end
+						if #pos_filters > 0 then
+							local longest = pos_filters[1]
+							for _, f in pairs(pos_filters) do
+								if f:len() > longest:len() then
+									longest = f
+								end
+							end
+							return longest
+						end
+					end
+
+					return vim.fn.getcwd(win)
+				end,
+			},
+		},
+	},
+	dependencies = {
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+		"nvim-tree/nvim-web-devicons",
+	},
 }
